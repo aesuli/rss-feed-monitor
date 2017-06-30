6657299f86a4851e7f43595cf487454fda2ba79e
Andrea Esuli <andrea@esuli.it>
2017-06-30 16:42:42 +0200

    bug fix

--- text_cleaner.py
+++ text_cleaner.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     for root, dirs, files in os.walk(args.input_dir):
         for file in files:
             if file.endswith(TEXT_EXT):
-                output_dir = os.path.join(args.output_dir, root[len(args.input_dir) + 1:])
+                output_dir = os.path.join(args.output_dir, root[len(args.input_dir):])
                 output_file = os.path.join(output_dir, file)
                 if not os.path.exists(output_file) or args.overwrite:
                     os.makedirs(output_dir, exist_ok=True)
